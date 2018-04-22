26a60dfedc53c81687a52a8e33971d4f3ad1b6f6
Robin van der Vleuten <robin@webstronauts.co>
2018-04-22 13:53:25 +0200

    Pass all prefixed env vars to the HTML template

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ require('dotenv').config();
 const resolveEnvVars = require('resolve-env-vars');
 const { DefinePlugin } = require('webpack');
 
-module.exports = config => {
-  const env = resolveEnvVars('PREACT_APP_');
-  config.plugins.push(new DefinePlugin(env.stringified));
+module.exports = (config, env, helpers) => {
+  const { stringified, raw } = resolveEnvVars('PREACT_APP_');
+  config.plugins.push(new DefinePlugin(stringified));
+
+  const { plugin: htmlPlugin } = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0]
+
+  if (htmlPlugin) {
+    // Pass all prefixed env vars to the HTML template.
+    htmlPlugin.options.env = raw
+  }
 };
