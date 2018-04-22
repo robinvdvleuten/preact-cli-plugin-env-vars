054948216e8467772b0e57aeccaf9f95611b834e
Robin van der Vleuten <robin@webstronauts.co>
2018-04-22 13:49:30 +0200

    Using resolve-env-var package

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 require('dotenv').config();
 
+const resolveEnvVars = require('resolve-env-vars');
 const { DefinePlugin } = require('webpack');
 
-const getEnvVars = () => {
-  const raw = Object.keys(process.env)
-    .filter(key => /^PREACT_APP_/i.test(key))
-    .reduce(
-      (env, key) => Object.assign(env, { [key]: process.env[key] }),
-      {}
-    );
-
-  const stringified = {
-    'process.env': Object.keys(raw).reduce(
-      (env, key) => Object.assign(env, { [key]: JSON.stringify(raw[key]) }),
-      {}
-    )
-  };
-
-  return { raw, stringified };
-};
-
 module.exports = config => {
-  const env = getEnvVars();
+  const env = resolveEnvVars('PREACT_APP_');
   config.plugins.push(new DefinePlugin(env.stringified));
 };
