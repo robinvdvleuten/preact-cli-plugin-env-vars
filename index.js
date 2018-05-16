8f086b9cae4dd6c136cf7273b78316c31ab4008b
Robin van der Vleuten <robin@webstronauts.co>
2018-05-16 09:16:14 +0200

    Fallback to empty object when plugin is undefined. Fixes #2.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = (config, env, helpers) => {
   const { stringified, raw } = resolveEnvVars('PREACT_APP_');
   config.plugins.push(new DefinePlugin(stringified));
 
-  const { plugin: htmlPlugin } = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0];
+  const { plugin: htmlPlugin } = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0] || {};
 
   if (htmlPlugin) {
     // Pass all prefixed env vars to the HTML template.
